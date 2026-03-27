2482cb7fb6a9d4ff1843ce60a6a862838659c1fd
Guilherme Ledes <ledes.guilherme@gmail.com>
2026-03-27 19:02:13 +0000

    feat: implement queue using two stacks

--- studies/problems/queue-using-two-stacks/solution.ts
+++ studies/problems/queue-using-two-stacks/solution.ts
@@ -7,22 +7,59 @@
 
 export type QueueQuery = readonly [1, number] | readonly [2] | readonly [3];
 
-const notImplementedMessage =
-  "Not implemented on main; use the solve/queue-using-two-stacks worktree.";
-
 export function processQueueUsingTwoStacksBrute(queries: readonly QueueQuery[]): number[] {
-  void queries;
-  throw new Error(notImplementedMessage);
+  const printResult: number[] = [];
+  const queue: number[] = [];
+
+  const queryProcessor = {
+    1: (x: number) => queue.push(x),
+    2: () => queue.shift(),
+    3: () => printResult.push(queue[0]!),
+  };
+
+  for (const message of queries) {
+    queryProcessor[message[0]](message[1]!);
+  }
+
+  return printResult;
 }
 
 export function processQueueUsingTwoStacksOptimized(
   queries: readonly QueueQuery[],
 ): number[] {
-  void queries;
-  throw new Error(notImplementedMessage);
+  const printResult: number[] = [];
+  const inStack: number[] = [];
+  const outStack: number[] = [];
+
+  const refillOutStack = () => {
+    if (outStack.length > 0) {
+      return;
+    }
+
+    while (inStack.length > 0) {
+      outStack.push(inStack.pop()!);
+    }
+  };
+
+  for (const message of queries) {
+    if (message[0] === 1) {
+      inStack.push(message[1]);
+      continue;
+    }
+
+    refillOutStack();
+
+    if (message[0] === 2) {
+      outStack.pop();
+      continue;
+    }
+
+    printResult.push(outStack[outStack.length - 1]!);
+  }
+
+  return printResult;
 }
 
 export function processQueueUsingTwoStacks(queries: readonly QueueQuery[]): number[] {
-  void queries;
-  throw new Error(notImplementedMessage);
+  return processQueueUsingTwoStacksOptimized(queries);
 }
